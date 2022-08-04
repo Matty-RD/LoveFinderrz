369398397bfeefdfa4ebb057bdc965582283bf6a
Matthew <mrdicke22@icloud.com>
2022-08-04 17:41:58 +0000

    demo login

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -26,6 +26,16 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
+  const demoUser = async (e) => {
+    e.preventDefault();
+    const DemoEmail = "demo@aa.io"
+    const DemoPassword = "password"
+    const data = await dispatch(login(DemoEmail, DemoPassword));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
   if (user) {
     return <Redirect to='/' />;
   }
@@ -57,6 +67,7 @@ const LoginForm = () => {
           onChange={updatePassword}
         />
         <button type='submit'>Login</button>
+        <button type='submit' className="button" onClick={demoUser}>Demo User</button>
       </div>
     </form>
   );
