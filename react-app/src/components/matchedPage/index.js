7bb8aa40111f7ae79e40fee9f47c7ba152fb1060
Matthew <mrdicke22@icloud.com>
2022-08-07 15:28:06 -0500

    Match buttons fixed

--- react-app/src/components/matchedPage/index.js
+++ react-app/src/components/matchedPage/index.js
@@ -1,32 +1,21 @@
-import { getAllMatchesThunk, updateMatchThunk, deleteMatchesThunk } from "../../store/match";
+import { getAllMatchesThunk, deleteMatchesThunk } from "../../store/match";
 import { useDispatch, useSelector} from "react-redux";
 import { useEffect} from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import './matchPage.css'
 
-function MatchesPage() {
+function MatchedPage() {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const sessionUser = useSelector(state => state.session.user);
     const matchesObject = useSelector((state) => state.matches);
     const matches = Object.values(matchesObject);
-    const usersMatches = matches.filter(match => match.second_userId === sessionUser.id && match.matched === false)
+    const usersMatches = matches.filter(match => match.second_userId === sessionUser.id && match.matched === true)
 
     useEffect(() => {
         dispatch(getAllMatchesThunk());
       }, [dispatch]);
-
-    const matchButton = (e) => {
-      e.preventDefault();
-      const id = e.target.id
-      const matched = true;
-      console.log(usersMatches)
-      const updateMatch = {
-        matched,
-      };
-      dispatch(updateMatchThunk(updateMatch, id));
-    }
-
 
     const deleteM = (e) => {
       e.preventDefault();
@@ -34,20 +23,22 @@ function MatchesPage() {
       dispatch(deleteMatchesThunk(id))
     }
 
+    const messages = (e) => {
+      e.preventDefault();
+      history.push('/messages')
+    }
 
     if(usersMatches.length === 0) {
       return (
         <div className="noMatches">
-          <h1>Sorry, no matches.</h1>
-          <img alt='' src={'https://c.tenor.com/B9OjyPMq5pIAAAAC/fine-this-is-fine.gif'}/>
           <h2>Maybe try making some new posts!</h2>
         </div>
       )
-    } else if(usersMatches.length > 0) {
+    } else if(matches.length > 0) {
       return (
         <>
         <div>
-          <h1>Here are your possible Matches!</h1>
+          <h1>List of Matched Users!</h1>
           <p>Feel free to view their page by clicking their name!</p>
         {usersMatches.map(filteredMatch  =>{
           return (
@@ -55,8 +46,8 @@ function MatchesPage() {
             <h1><NavLink to={`/users/${filteredMatch.id}`}>{filteredMatch.liker.username}</NavLink></h1>
             <img alt='' className="profilepicture" src={filteredMatch.liker.profile_pic}/>
             <div className="buttonDiv">
-            <button type='submit' id={filteredMatch.id} onClick={matchButton}>Match?</button>
-            <button type='submit' id={filteredMatch.id} onClick={deleteM}>Pass?</button>
+            <button type='submit' id={filteredMatch.id} onClick={messages}>Message</button>
+            <button type='submit' id={filteredMatch.id} onClick={deleteM}>Unmatch</button>
             </div>
             </div>
             )
@@ -68,4 +59,4 @@ function MatchesPage() {
   }
 
 
-  export default MatchesPage;
+  export default MatchedPage;
